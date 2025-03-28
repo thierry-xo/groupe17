971505dce393a9e2c8b2c8f155a74e4b52bfa716
Lionelfj02 <159890499+Lionelfj02@users.noreply.github.com>
2025-03-28 17:02:26 +0100

    premier commit

--- TP17/Program.cs
+++ TP17/Program.cs
@@ -19,8 +19,8 @@ namespace TP2
 
             Console.WriteLine("ABSTRACT CLASS");
             Console.WriteLine("===============\n");
-            Teacher t1 = new Teacher("longa", "lalonga", "juufe788");
-            Student s1 = new Student("maki", "mak", "22lsi65774");
+            Teacher t1 = new Teacher("longa", "ndaye", "tambwe");
+            Student s1 = new Student("maki", "mak", "22lsi65774")
 
             t1.Add(2);
             s1.Add(2);
