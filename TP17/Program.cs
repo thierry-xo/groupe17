d8317c267edeac487e42f8a7a637179f4efc168b
vivianemulenda <nawilelaviviane@gmail.com>
2025-03-29 10:32:53 +0100

    Update Program.cs
    commit deuxième

--- TP17/Program.cs
+++ TP17/Program.cs
@@ -19,7 +19,7 @@ namespace TP2
 
             Console.WriteLine("ABSTRACT CLASS");
             Console.WriteLine("===============\n");
-            Teacher t1 = new Teacher("longa", "ndaye", "tambwe");
+            Teacher t1 = new Teacher("thierry", "nirere", "sebagenzi");
             Student s1 = new Student("maki", "mak", "22lsi65774")
 
             t1.Add(2);
