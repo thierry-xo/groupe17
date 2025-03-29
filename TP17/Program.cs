8bbbd85e27e12f86756f5fd591ac9df0875dc025
NGUMBINGENGELEANTOINE <antoinengumbi26@gmail.com>
2025-03-29 11:55:52 +0100

    Update Program.cs
    commit antoine

--- TP17/Program.cs
+++ TP17/Program.cs
@@ -19,7 +19,7 @@ namespace TP2
 
             Console.WriteLine("ABSTRACT CLASS");
             Console.WriteLine("===============\n");
-            Teacher t1 = new Teacher("longa", "lalonga", "juufe788");
+            Teacher t1 = new Teacher("ANTOINE", "lUNGELE", "juufe788");
             Student s1 = new Student("maki", "mak", "22lsi65774");
 
             t1.Add(2);
